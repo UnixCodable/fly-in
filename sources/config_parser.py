8da2bbfda2a3febb88c49db7853f053daa15b264
UnixCodable <lucasbordanave@gmail.com>
2026-06-02 12:17:02 +0200

    feat: added base files

--- sources/config_parser.py
+++ sources/config_parser.py
@@ -23,16 +23,35 @@ class LineParser(BaseModel, validate_assignment=True):
     lines: List[List[str]] = Field()
 
     @model_validator(mode='after')
+    def invalid_values(self):
+        
+
+    @model_validator(mode='after')
+    def number_of_drones(self):
+        if self.lines[0][0] == 'nb_drones':
+            if 'nb_drones' not in [li[0] for li in self.lines[1:]]:
+                return self
+        raise ValueError('There must be only one nb_drones value and it must be at the first line of the config file.')
+
+    @model_validator(mode='after')
     def start_parsing(self):
-        hub_list = [line for line in self.lines if line[0] == "start_hub"]
+        hub_list = [li for li in self.lines if li[0] == "start_hub"]
         if len(hub_list) != 1:
             raise ValueError('There must be only one starting hub.')
+        return self
+
+    @model_validator(mode='after')
+    def end_parsing(self):
+        hub_list = [li for li in self.lines if li[0] == "end_hub"]
+        if len(hub_list) != 1:
+            raise ValueError('There must be only one starting hub.')
+        return self
 
 
 def read_map() -> List:
     lines = []
     temp = []
-    with open("maps/hard/03_ultimate_challenge.txt") as file:
+    with open("assets/maps/hard/03_ultimate_challenge.txt") as file:
         for line in file.readlines():
             try:
                 line = line[0:line.index("#")]
@@ -43,12 +62,5 @@ def read_map() -> List:
             temp = line.split(':')
             temp[0] = temp[0].strip()
             temp[1] = temp[1].strip()
-            print(temp)
             lines.append(temp)
         return lines
-
-
-try:
-    object = LineParser(lines=read_map())
-except ValidationError as err:
-    print(err)
--- sources/main.py
+++ sources/main.py
@@ -1,12 +1,15 @@
 # ************************************************************************* #
 #                                                                           #
 #                                                      :::      ::::::::    #
-#  Makefile                                          :+:      :+:    :+:    #
+#  main.py                                           :+:      :+:    :+:    #
 #                                                  +:+ +:+         +:+      #
 #  By: lbordana <lbordana@student.42mulhouse.f   +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
-#  Created: 2026/06/02 08:09:16 by lbordana        #+#    #+#               #
-#  Updated: 2026/06/02 08:09:20 by lbordana        ###   ########.fr        #
+#  Created: 2026/06/02 10:40:23 by lbordana        #+#    #+#               #
+#  Updated: 2026/06/02 11:07:58 by lbordana        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
+from config_parser import read_map, LineParser
+
+object = LineParser(lines=read_map())
