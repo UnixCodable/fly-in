516e71bfeb55030b7d3c1e1e4c6b49d6c875bbbe
Lucas Bordanave <lbordana@k1r3p2.42mulhouse.fr>
2026-06-09 18:31:31 +0200

    fix: wip on parsing

--- sources/config_parser.py
+++ sources/config_parser.py
@@ -10,56 +10,41 @@
 #                                                                             #
 # *************************************************************************** #
 
-from pydantic import (
-    BaseModel,
-    Field,
-    ValidationError,
-    field_validator,
-    )
-from typing import List, Any
+from pydantic import BaseModel, Field, ValidationError, field_validator
+# from typing import List, Any
 import sys
 
+class Drone(BaseModel):
+    number:      int = Field(gt=0)
+    config_line: int = Field(ge=0)
 
-class ConfigParser(BaseModel):
-    keys: list[str]
-    hub_name: list[str]
-    hub_coordinates: list[tuple[int, int]]
-    hub_metadata: Any
-    nb_drones: int = Field(gt=0)
 
-    @field_validator('hub_name', mode='after')
+class Connection(BaseModel):
+    first_zone:        str = Field(max_length=30)
+    second_zone:       str = Field(max_length=30)
+    max_link_capacity: int = Field(default=1, gt=0)
+    config_line:       int = Field(ge=0)
+
+
+class Hub(BaseModel):
+    coordinates: tuple[int, int]
+    name:        str = Field(max_length=30)
+    color:       str = Field(default='black')
+    zone:        str = Field(default='normal')
+    max_drones:  int = Field(default=1, gt=0)
+    config_line: int = Field(ge=0)
+
+    @field_validator('name', mode='after')
     @classmethod
-    def validate_hub_name(cls, v):
-        if len(set(v)) != len(v):
-            raise ValueError('Hub names cannot be duplicated')
-        return v
-
-    @field_validator('keys', mode='after')
-    @classmethod
-    def validate_keys(cls, v):
-
-        start_list = [key for key in v if key == "start_hub"]
-        end_list = [key for key in v if key == "end_hub"]
-        accredited = {'connection', 'hub', 'start_hub', 'end_hub', 'nb_drones'}
-
-        for key in v:
-            if key not in accredited:
-                raise ValueError('Key can only contain "hub", "start_hub",'
-                                 '"end_hub", "connection", "nb_drones".')
-        if v[0] != 'nb_drones':
-            raise ValueError('No nb_drones at first line of config.')
-        if 'nb_drones' in [k for k in v[1:]]:
-            raise ValueError('There must be only one nb_drones value.')
-        if len(start_list) != 1:
-            raise ValueError('There must be only one starting hub.')
-        if len(end_list) != 1:
-            raise ValueError('There must be only one ending hub.')
-
-        return v
+    def validate_name(cls, name):
+        if "".join(name.split('_')).isalnum() is False:
+            raise ValueError(f"Name {name} must only contain alphanumeric and"
+                             "underscore characters")
+        return name
 
 
 def read_map() -> list:
-    values = []
+    hub, connection, drone = ([], [], [])
     with open("assets/maps/hard/03_ultimate_challenge.txt") as file:
         for nb, line in enumerate(file.readlines()):
             line = line[0:line.index("#") if '#' in line else -1]
@@ -67,23 +52,22 @@ def read_map() -> list:
             if line == '\n' or line == '':
                 continue
             if ' ' in line[0] or '  ' in line or '::' in line:
-                raise ValueError(f'Error: separators on line {nb + 1}')
+                raise ValueError(f"(line {nb + 1}) : Wrong separators")
 
-            temp = line.split(':', maxsplit=1)[:1] + line.split()[1:]
-            
-    return values
+            key, sep, val = line.partition(':')
+            if not sep:
+                raise ValueError(f"(line {nb + 1}) : Missing ':'")
+            if not val:
+                raise ValueError(f"(line {nb + 1}) : Missing values")
+            line = line.split(':')[:1] + line.split()[1:]
+            print(line)
+    return (hub, connection, drone)
 
 
-def parse_map() -> ConfigParser:
+def parse_map():
     try:
-        values = read_map()
-        parsed = ConfigParser(
-            keys=[k[0] for k in values],
-            nb_drones=values[0][1],
-            hub_name=[v[1] for v in values if 'hub' in v[0]],
-            hub_coordinates=[(v[2], v[3]) for v in values if 'hub' in v[0]],
-            hub_metadata=[v[4] for v in values if 'hub' in v[0]]
-        )
+        hub, connection, drone = read_map()
+        # connection = Connections()
     except ValidationError as err:
         for e in err.errors():
             print(e.get('msg'))
@@ -93,7 +77,7 @@ def parse_map() -> ConfigParser:
         print(err)
         print('Please ensure format is "<key>: <value> ... <[metadata]>."')
         sys.exit(0)
-    return parsed
+    return
 
 
         #     if ' ' in line[0]:
@@ -134,3 +118,31 @@ def parse_map() -> ConfigParser:
         #              temp[1] if len(temp) > 1 else None]
         #              )
         # print(values)
+
+    #     class Config(BaseModel):
+    # keys:        list[str]
+    # hub:         list[Hub]
+    # connections: list[Connections]
+
+    # @field_validator('keys', mode='after')
+    # @classmethod
+    # def validate_keys(cls, v):
+
+    #     start_list = [key for key in v if key == "start_hub"]
+    #     end_list = [key for key in v if key == "end_hub"]
+    #     accredited = {'connection', 'hub', 'start_hub', 'end_hub', 'nb_drones'}
+
+    #     for key in v:
+    #         if key not in accredited:
+    #             raise ValueError('Key can only contain "hub", "start_hub",'
+    #                                 '"end_hub", "connection", "nb_drones".')
+    #     if v[0] != 'nb_drones':
+    #         raise ValueError('No nb_drones at first line of config.')
+    #     if 'nb_drones' in [k for k in v[1:]]:
+    #         raise ValueError('There must be only one nb_drones value.')
+    #     if len(start_list) != 1:
+    #         raise ValueError('There must be only one starting hub.')
+    #     if len(end_list) != 1:
+    #         raise ValueError('There must be only one ending hub.')
+
+    #     return v
