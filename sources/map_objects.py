3aa31ad6f16472c5cff1ef23b2630ab64749dd27
UnixCodable <lucasbordanave@gmail.com>
2026-06-16 10:00:19 +0200

    feat: wip config_parser

--- sources/map_objects.py
+++ sources/map_objects.py
@@ -6,7 +6,7 @@
 #   By: lbordana <lbordana@student.42mulhouse.fr>   +#+  +:+       +#+        #
 #                                                 +#+#+#+#+#+   +#+           #
 #   Created: 2026/06/10 17:43:11 by lbordana           #+#    #+#             #
-#   Updated: 2026/06/15 23:17:49 by lbordana          ###   ########.fr       #
+#   Updated: 2026/06/16 09:49:00 by lbordana          ###   ########.fr       #
 #                                                                             #
 # *************************************************************************** #
 
@@ -19,34 +19,17 @@ class Drone(BaseModel):
 
 
 class Connection(BaseModel):
-    first_zone:  str = Field(max_length=30)
-    second_zone: str = Field(max_length=30)
+    first_zone:  str = Field(max_length=30, pattern=r"^([a-zA-Z0-9_]+)$")
+    second_zone: str = Field(max_length=30, pattern=r"^([a-zA-Z0-9_]+)$")
     max_link:    int = Field(ge=0)
     line:        int = Field(ge=0)
-
-    @model_validator(mode='after')
-    def validate_zone(self):
-        if "".join(self.first_zone.split('_')).isalnum() is False:
-            raise ValueError(f"(line {self.line}) Zone {self.first_zone} must only contain alphanumeric or"
-                             " underscore characters")
-        if "".join(self.second_zone.split('_')).isalnum() is False:
-            raise ValueError(f"(line {self.line}) Zone {self.second_zone} must only contain alphanumeric or"
-                             " underscore characters")
-        return self
 
 
 class Hub(BaseModel):
     hub_type:    str
-    name:        str = Field(max_length=30)
+    name:        str = Field(max_length=30, pattern=r"^([a-zA-Z0-9_]+)$")
     coordinates: tuple[int, int]
     color:       str = Field(pattern=r"^([a-zA-Z]+)$")
-    zone:        str
+    zone:        str = Field(pattern=r"(restricted|priority|normal|blocked)")
     max_drones:  int = Field(ge=0)
     line: int = Field(ge=0)
-
-    @model_validator(mode='after')
-    def validate_name(self):
-        if "".join(self.name.split('_')).isalnum() is False:
-            raise ValueError(f"(line {self.line}) Name {self.name} must only contain alphanumeric or"
-                             " underscore characters")
-        return self
